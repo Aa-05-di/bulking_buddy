5279c00bbaa41fcb68b9b527e81fdd507dcbaff3
Aa-05-di <aadithyanpramad@gmail.com>
2025-07-19 08:43:31 +0530

    log2

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -27,3 +27,30 @@ Future<String> registerUser({
     return jsonDecode(response.body)['message'];
   }
 }
+
+Future<String>loginUser({
+  required String email,
+  required String password
+})async{
+  final url = Uri.parse("$baseurl/login");
+
+  final response = await http.post(
+    url,
+    headers: {"Content-Type":"application/json"},
+    body:jsonEncode({
+      "username":email,
+      "password":password,
+    })
+  );
+  try {
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      return data['message'];
+    } else {
+      final data = jsonDecode(response.body);
+      return data['message'] ?? "Login failed";
+    }
+  } catch (e) {
+    return "Unexpected error: ${response.body}";
+  }
+}
